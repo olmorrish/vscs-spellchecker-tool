fac043a3dbc8536a8fef82f12b1b75bba5575107
Oliver Morrish <olmorrish@gmail.com>
2023-10-14 12:47:38 -0600

    Import npm spellchecker

---
+++ index.js
@@ -0,0 +1,4 @@
+const projectResourcesPath = "C:\\Users\\olive\\Documents\\Unity Projects\\VSCS-2\\Assets\\Resources";
+const convosPath = `${projectResourcesPath}\\Convos_VSCS-II`
+const articlesPath = `${projectResourcesPath}\\Articles`
+const textFilesPath = `${projectResourcesPath}\\TextFiles`
