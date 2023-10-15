6222203d999b7819d32c22aad0f0aedc2bf3fb15
Oliver Morrish <olmorrish@gmail.com>
2023-10-15 13:33:06 -0600

    Basic spellcheck on articles

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 import Spellchecker from "spellchecker";
 import {getAllFilesInDirectory} from './utils.js';
+import fs from 'fs';
 
 const projectResourcesPath = "C:\\Users\\olive\\Documents\\Unity Projects\\VSCS-2\\Assets\\Resources";
-const convosPath = `${projectResourcesPath}\\Convos_VSCS-II`;
-const articlesPath = `${projectResourcesPath}\\Articles`;
-const textFilesPath = `${projectResourcesPath}\\TextFiles`;
+const allConvosDir = `${projectResourcesPath}\\Convos_VSCS-II`;
+const allArticlesDir = `${projectResourcesPath}\\Articles`;
+const allTextFilesDir = `${projectResourcesPath}\\TextFiles`;
 
 const customWords = ["Arbough"]; //TODO add custom ignorelist
 customWords.forEach(word => Spellchecker.add(word));
 
-const allConvoPaths = getAllFilesInDirectory(convosPath);
-const allArticlePaths = getAllFilesInDirectory(articlesPath);
-const allTextFilePaths = getAllFilesInDirectory(textFilesPath);
+const allConvoNames = getAllFilesInDirectory(allConvosDir);
+const allArticleNames = getAllFilesInDirectory(allArticlesDir);
+const allTextFileNames = getAllFilesInDirectory(allTextFilesDir);
 
-console.log('List of files in the directory:', allTextFilePaths);
+//Check all article files
+allTextFileNames.forEach(fileName => {
+    //Get the path for the file, then read its contents
+    const filePath = `${allTextFilesDir}\\${fileName}`
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
 
-const a = Spellchecker.isMisspelled('beamns');
-console.log(a);
+    //TODO spell check
+    const arrayOfErrorIndices = Spellchecker.checkSpelling(fileContent);
+
+    //ex
+    if(arrayOfErrorIndices){
+
+        arrayOfErrorIndices.forEach(errorRange => {
+            console.log(fileContent.substring(errorRange.start, errorRange.end));
+        });
+
+    }
+})
