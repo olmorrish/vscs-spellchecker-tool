a62e0b7ec9fc391dc39bb25802ec0809534c113b
Oliver Morrish <olmorrish@gmail.com>
2023-10-15 13:54:35 -0600

    Print out spellcheck report for articles

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ const allConvosDir = `${projectResourcesPath}\\Convos_VSCS-II`;
 const allArticlesDir = `${projectResourcesPath}\\Articles`;
 const allTextFilesDir = `${projectResourcesPath}\\TextFiles`;
 
-const customWords = ["Arbough"]; //TODO add custom ignorelist
-customWords.forEach(word => Spellchecker.add(word));
+const convoReportFileName = "convoReport.txt";
+const articleReportFileName = "articleReport.txt";
+const textFileReportFileName = "textFileReport.txt";
+
+// Clear report files and set up data objects
+fs.writeFileSync(convoReportFileName, '', 'utf-8');
+fs.writeFileSync(articleReportFileName, '', 'utf-8');
+fs.writeFileSync(textFileReportFileName, '', 'utf-8');
+
+//const customWords = fs.readFileSync('dictionary.txt', 'utf-8').split('\n').map(word => word.trim());
+//customWords.forEach(word => Spellchecker.add(word));
 
 const allConvoNames = getAllFilesInDirectory(allConvosDir);
 const allArticleNames = getAllFilesInDirectory(allArticlesDir);
@@ -16,19 +25,35 @@ const allTextFileNames = getAllFilesInDirectory(allTextFilesDir);
 
 //Check all article files
 allTextFileNames.forEach(fileName => {
+    let articleTypos = [];
+
     //Get the path for the file, then read its contents
     const filePath = `${allTextFilesDir}\\${fileName}`
     const fileContent = fs.readFileSync(filePath, 'utf-8');
 
-    //TODO spell check
+    //Scan, then extract all flagged words from text
     const arrayOfErrorIndices = Spellchecker.checkSpelling(fileContent);
-
-    //ex
     if(arrayOfErrorIndices){
-
         arrayOfErrorIndices.forEach(errorRange => {
-            console.log(fileContent.substring(errorRange.start, errorRange.end));
+            const misspelledWord = fileContent.substring(errorRange.start, errorRange.end);
+            articleTypos.push({
+                "word": misspelledWord,
+                "startIndex": errorRange.start,
+                "endIndex": errorRange.end,
+            })
+        });
+    }
+
+    //Write the data for this file
+    if(articleTypos.length > 0) {
+        
+        fs.appendFileSync(articleReportFileName, `----- ${fileName.toUpperCase()} -----\n`, 'utf-8')
+
+        articleTypos.forEach(typoData => {
+            const possibleCorrections = Spellchecker.getCorrectionsForMisspelling(typoData.word);
+            fs.appendFileSync(articleReportFileName, `${typoData.word}... did you mean: ${possibleCorrections}?\n`, 'utf-8');
         });
 
+        fs.appendFileSync(articleReportFileName, '\n\n', 'utf-8')
     }
 })
