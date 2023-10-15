bad326cd91501ee4e8507fc1a162a82e9a17a0b7
Oliver Morrish <olmorrish@gmail.com>
2023-10-15 14:05:27 -0600

    Articles + TextFiles scanning done

--- index.js
+++ index.js
@@ -16,16 +16,58 @@ fs.writeFileSync(convoReportFileName, '', 'utf-8');
 fs.writeFileSync(articleReportFileName, '', 'utf-8');
 fs.writeFileSync(textFileReportFileName, '', 'utf-8');
 
-//const customWords = fs.readFileSync('dictionary.txt', 'utf-8').split('\n').map(word => word.trim());
-//customWords.forEach(word => Spellchecker.add(word));
+const customWords = fs.readFileSync('dictionary.txt', 'utf-8').split('\n').map(word => word.trim());
+customWords.forEach(word => Spellchecker.add(word));
 
 const allConvoNames = getAllFilesInDirectory(allConvosDir);
 const allArticleNames = getAllFilesInDirectory(allArticlesDir);
 const allTextFileNames = getAllFilesInDirectory(allTextFilesDir);
 
-//Check all article files
-allTextFileNames.forEach(fileName => {
+/**
+ * 1. Check all convo files
+ */
+
+//TODO
+
+/**
+ * 2. Check all article files
+ */
+allArticleNames.forEach(fileName => {
     let articleTypos = [];
+
+    //Get the path for the file, then read its contents
+    const filePath = `${allArticlesDir}\\${fileName}`
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+
+    //Scan, then extract all flagged words from text
+    const arrayOfErrorIndices = Spellchecker.checkSpelling(fileContent);
+    if(arrayOfErrorIndices){
+        arrayOfErrorIndices.forEach(errorRange => {
+            const misspelledWord = fileContent.substring(errorRange.start, errorRange.end);
+            articleTypos.push({
+                "word": misspelledWord,
+                "start": errorRange.start,
+                "end": errorRange.end,
+            })
+        });
+    }
+
+    //Write the data for this file under a header
+    if(articleTypos.length > 0) {
+        fs.appendFileSync(articleReportFileName, `----- ${fileName.toUpperCase()} -----\n`, 'utf-8')
+        articleTypos.forEach(typoData => {
+            const possibleCorrections = Spellchecker.getCorrectionsForMisspelling(typoData.word);
+            fs.appendFileSync(articleReportFileName, `"${typoData.word}" ----- (index: ${typoData.start}) Did you mean: ${possibleCorrections}?\n`, 'utf-8');
+        });
+        fs.appendFileSync(articleReportFileName, '\n\n', 'utf-8')
+    }
+})
+
+/**
+ * 3. Check all article files
+ */
+allTextFileNames.forEach(fileName => {
+    let textFileTypos = [];
 
     //Get the path for the file, then read its contents
     const filePath = `${allTextFilesDir}\\${fileName}`
@@ -36,24 +78,21 @@ allTextFileNames.forEach(fileName => {
     if(arrayOfErrorIndices){
         arrayOfErrorIndices.forEach(errorRange => {
             const misspelledWord = fileContent.substring(errorRange.start, errorRange.end);
-            articleTypos.push({
+            textFileTypos.push({
                 "word": misspelledWord,
-                "startIndex": errorRange.start,
-                "endIndex": errorRange.end,
+                "start": errorRange.start,
+                "end": errorRange.end,
             })
         });
     }
 
-    //Write the data for this file
-    if(articleTypos.length > 0) {
-        
-        fs.appendFileSync(articleReportFileName, `----- ${fileName.toUpperCase()} -----\n`, 'utf-8')
-
-        articleTypos.forEach(typoData => {
+    //Write the data for this file under a header
+    if(textFileTypos.length > 0) {
+        fs.appendFileSync(textFileReportFileName, `----- ${fileName.toUpperCase()} -----\n`, 'utf-8')
+        textFileTypos.forEach(typoData => {
             const possibleCorrections = Spellchecker.getCorrectionsForMisspelling(typoData.word);
-            fs.appendFileSync(articleReportFileName, `${typoData.word}... did you mean: ${possibleCorrections}?\n`, 'utf-8');
+            fs.appendFileSync(textFileReportFileName, `"${typoData.word}" ----- (index: ${typoData.start}) Did you mean: ${possibleCorrections}?\n`, 'utf-8');
         });
-
-        fs.appendFileSync(articleReportFileName, '\n\n', 'utf-8')
+        fs.appendFileSync(textFileReportFileName, '\n\n', 'utf-8')
     }
 })
